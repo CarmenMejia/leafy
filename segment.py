980c54e2c1488d4d991c48c1f90481f54a9d2b4b
CarmenMejia <carmen.r.mejia.1@gmail.com>
2016-12-02 01:15:37 -0800

    trying to segment
    failing

--- segment.py
+++ segment.py
@@ -1,26 +1,41 @@
 import numpy as np
-import cv2
+import cv2, os, sys
 from matplotlib import pyplot as plt
 
 
 
-for subdir, dirs, files in os.walk(os.path.join('..', experiment_dir)):
+try:
+    image_dir = os.path.abspath(sys.argv[1])
+except:
+    print "\nUSAGE: python segment.py image_dir\n"
+    sys.exit()
+
+
+out_dir = "segmented"
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+# os.chdir(out_dir)
+
+for subdir, dirs, files in os.walk(os.path.join('..', image_dir)):
     for file in files:
         filename, ext = os.path.splitext(file)
-        path_prefix = os.path.split(experiment_dir)[-1]
+        path_prefix = os.path.split(image_dir)[-1]
+
         # note: opencv doesn't support PDFs so those need to be changed to images
         if ext == '.tif' or ext == '.jpg':
-            # print filename
-            # make a directory for the image
-            if not os.path.exists(filename):
-                os.mkdir(filename)
-            os.chdir(filename)
-
             # read the image and run the given function on it
             img = cv2.imread(os.path.join(subdir, file))
-            cv2.imwrite(file, img)
 
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            print gray
+            
+            # thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+            # thresh = cv2.medianBlur(thresh,5)
 
-img = cv2.imread('coins.png')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            # ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+            # read the image and run the given function on it
+            # img = cv2.imread(os.path.join(subdir, file))
+            # print (os.path.join(out_dir,file))
+
+            # cv2.imwrite(os.path.join(out_dir, file), thresh)
