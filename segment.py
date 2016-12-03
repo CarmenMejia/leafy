d2d9ddff1241e9f270e6ce421547ffa431d94bd7
CarmenMejia <carmen.r.mejia.1@gmail.com>
2016-12-03 02:15:30 -0800

    segmenting works :D

--- segment.py
+++ segment.py
@@ -1,6 +1,62 @@
 import numpy as np
 import cv2, os, sys
-from matplotlib import pyplot as plt
+import matplotlib as plt
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+# modified from Stanford 2013
+def kmeans(dataSet, k):
+    numFeatures = 2
+    centroids = getRandomCentroids(numFeatures, k)
+    # Initialize book keeping vars.
+    iterations = 0
+    oldCentroids = None
+
+    # Run the main k-means algorithm
+    while not shouldStop(oldCentroids, centroids, iterations):
+        # Save old centroids for convergence test. Book keeping.
+        oldCentroids = centroids
+        iterations += 1
+
+        # Assign labels to each datapoint based on centroids
+        labels = getLabels(dataSet, centroids)
+
+        # Assign centroids based on datapoint labels
+        centroids = getCentroids(dataSet, labels, k)
+
+    # We can get the labels too by calling getLabels(dataSet, centroids)
+    return centroids
+
+
+
+def shouldStop(oldCentroids, centroids, iterations):
+    if iterations > MAX_ITERATIONS: return True
+    return oldCentroids == centroids
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def kmeans():
+    digits = load_digits(2)
+    data = scale(digits.datda)
+
+    n_samples, n_features = data.shape
+    n_digits = len(np.unique(digits.target))
+    labels = digits.target
+
 
 
 
@@ -26,16 +82,40 @@ for subdir, dirs, files in os.walk(os.path.join('..', image_dir)):
             # read the image and run the given function on it
             img = cv2.imread(os.path.join(subdir, file))
 
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            print gray
-            
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # s = hsv[:,:,1]
+            # v = hsv[:,:,2]
+
             # thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
             # thresh = cv2.medianBlur(thresh,5)
 
-            # ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            # ret, thresh = cv2.threshold(hsv[1],0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            # ret2, thresh = cv2.threshold(hsv[2],0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-            # read the image and run the given function on it
-            # img = cv2.imread(os.path.join(subdir, file))
-            # print (os.path.join(out_dir,file))
 
-            # cv2.imwrite(os.path.join(out_dir, file), thresh)
+            # print hsv[0].shape
+            height = hsv.shape[0]
+            width = hsv.shape[1]
+
+            hsv = hsv.reshape((height * width, 3))
+            s = hsv[:,1]
+            v = hsv[:,2]
+
+
+            sv = cv2.merge((s,v))
+            sv = sv.reshape((height * width), 2)
+            clt = KMeans(n_clusters = 2)
+            clt.fit(sv)
+
+            # reshaping the hsv back to the original height and width
+            thresh = hsv[:,1].reshape(height, width)
+
+            labels = clt.labels_
+            thresh = labels.reshape(height, width)
+            for i in range(0,height):
+                for j in range(0,width):
+                    if (thresh[i,j] == 1):
+                        thresh[i,j] = 255
+
+
+            cv2.imwrite(os.path.join(out_dir, file), thresh)
